660278d889cb0469b69c69c2d27092137fb7f593
andieljm <90933472+andieljm@users.noreply.github.com>
2022-06-13 00:44:12 -0600

    inicio de pruebas

---
+++ JavaApplication8/src/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+
+   static WebDriver driver;
+    public static void main(String[] args) {
+      
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Documents\\Drivers de crome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        
+        // buscar una pagina
+       
+    }
+    
+}
