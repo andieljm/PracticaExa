5c522fcbfcefdce4606d984c10d622945ca9df8f
andieljm <90933472+andieljm@users.noreply.github.com>
2022-06-24 19:02:16 -0600

    Tarea 3

---
+++ InitializrSpringbootProject/src/test/java/com/PracticaExa/PracticaExamen/PracticaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticaExa.PracticaExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
