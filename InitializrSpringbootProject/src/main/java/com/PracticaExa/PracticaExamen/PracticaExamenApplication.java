91deeaef04a9c42058f5f0e4f053416b7172e374
AndresCanul <98436130+AndresCanul@users.noreply.github.com>
2022-06-24 19:17:46 -0600

    Eliminacion

--- InitializrSpringbootProject/src/main/java/com/PracticaExa/PracticaExamen/PracticaExamenApplication.java
+++ InitializrSpringbootProject/src/main/java/com/PracticaExa/PracticaExamen/PracticaExamenApplication.java
@@ -8,7 +8,7 @@ public class PracticaExamenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticaExamenApplication.class, args);
-        System.out.println("Prueba");
+        System.out.println("Prggueba");
     }
 
 }
